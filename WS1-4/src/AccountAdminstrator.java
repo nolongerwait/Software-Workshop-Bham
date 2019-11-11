712dea81c0e81428d4a4b6d1d0d6ec6fc5e51c3a
xiaoyaoyou95@hotmail.com <xiaoyaoyou95@hotmail.com>
2019-11-11 17:58:38 +0000

    Finished the two sub-classes.

---
+++ WS1-4/src/AccountAdminstrator.java
@@ -0,0 +1,83 @@
+import java.util.ArrayList;
+
+/**
+ *  AccountAdminstrator is a sub-class of Account. An adminstrator can reset the accounts of standard users (in case they hae forgotten their password or have attempted to login unsuccessfully to often). An administrator can have arbitrarily many unsuccessful login attempts.
+ * 
+ *  @author Zetian Qin zxq876
+ *  @version 2019-11-11 17:34:53
+ */
+public class AccountAdminstrator extends Account implements AccountAdministratorInterface {
+    private ArrayList<Account> accounts;
+
+    /** 
+     * Standard constructor of the abstract class Account.
+     * @param name The name of the user of the account.
+     * @param salutation The salutation of the account holder (e.g., "Mr", "Ms", "Mrs", "Dr", "Prof")
+     * @param email The email address of the account holder.
+     * @param password The password of the account.
+     * @param loggedIn true if the user is looged in, false else.
+     * @param accounts The list of accounts the administrator looks after.
+     */
+    public AccountAdminstrator(String name, String salutation, String email, String password, boolean loggedIn, ArrayList<Account> accounts) {
+        super(name, salutation, email, password, loggedIn);
+        this.accounts = accounts;
+    }
+
+    /**
+     * Getter method to return the list of accounts the administrator looks after.
+     * @return The list of accounts the administrator looks after.
+     */
+    public ArrayList<Account> getAccounts() {
+        return this.accounts;
+    }
+
+    /**
+     *  The method adds an account to the list of accounts the administrator looks after.
+     *  @param customer The new account to be added to the list of accounts the administrator looks after.
+     */
+    public void addAccount(Account account) {
+        this.accounts.add(account);
+    }
+
+    /**
+     *  If an account can no longer be used, since either the user has forgotten their password or in case of a standard user has entered the password incorrectly too often the administrator can reset the account by setting a new password and resetting the number of failed login attempts to zero (the latter only if the account is a standard account).
+     *  @param account The account that is to be reset.
+     *  @param password The new password for the account that is to be reset.
+     */
+    public void resetAccount(Account account, String password) {
+        if(account instanceof AccountStandard) {
+            ((AccountStandard) account).setFailedLoginAttempts(0);
+            ((AccountStandard) account).setPassword(password);
+        }
+    }
+
+    /**
+     *  Method for an adminstrator to log in by providing a password. 
+     * It is checked whether the password provided is correct.
+     *  @param password The password provided for the login; this is to be compared to the password stored on the system.
+     */
+    public void login(String password) {
+        if(this.checkPassword(password)) {
+            this.setLoggedIn(true);
+        }
+        else {
+            this.setLoggedIn(false);
+        }
+    }
+
+    @Override
+    /**
+     * Standard toString method to represent the object in a human
+     * readable form. If the user is not logged in, only a warning
+     * should be printed, but no substantial information be given.
+     * @return The object in a human readable form.
+     */
+    public String toString() {
+        if(this.getLoggedIn()) {
+            return "AccountAdminstrator{" + this.getSalutation() + " " + this.getName() + ", \n You are logged in." + "}";
+        }
+        else {
+            return "No user log in!";
+        }
+    }
+}
---
+++ WS1-4/src/AccountStandard.java
@@ -0,0 +1,125 @@
+import java.util.ArrayList;
+
+/**
+ *  A standard account is an account for customers who can download music from the music web site. They are represented by 3 field variables: balance, titlesBought, and failedLoginAttempts of types int, ArrayList<MusicTitle>, and int, respectively. Only three login attempts are possible. If the password is entered 3 times incorrectly, the account is disabled and only an administrator can reinstate it.
+ * 
+ *  @author Zetian Qin zxq876
+ *  @version 2019-11-11 16:08:58
+ */
+public class AccountStandard extends Account implements AccountStandardInterface {
+    private int balance;
+    private ArrayList<MusicTitle> titlesBought;
+    private int failedLoginAttempts;
+    
+    public static final int MAXIMALLOGINATTEMPTS = 3;
+
+    /**
+     * Standard constructor of the abstract class AccountStandard.
+     * @param name The name of the user of the account.
+     * @param salutation The salutation of the account holder (e.g., "Mr", "Ms", "Mrs", "Dr", "Prof")
+     * @param email The email address of the account holder.
+     * @param password The password of the account.
+     * @param loggedIn true if the user is looged in, false else.
+     * @param balance The balance of the account.
+     * @param titlesBought The current list of titles bought by the user.
+     * @param failedLoginAttempts The number of failed login attempts.
+     */
+    public AccountStandard(String name, String salutation, String email, String password, boolean loggedIn, int balance, ArrayList<MusicTitle> titlesBought, int failedLoginAttempts) {
+        super(name, salutation, email, password, loggedIn);
+        this.balance = balance;
+        this.titlesBought = titlesBought;
+        this.failedLoginAttempts = failedLoginAttempts;
+    }
+
+    /**
+     * Getter method to return the balance.
+     * @return The balance of the account.
+     */
+    public int getBalance() {
+        return this.balance;
+    }
+
+    /**
+     *  Method for a user to log in to their account by providing a password. It is first checked whether the account is still active (that is, not too many failed login attempts were made in the past) and secondly whether the password provided is correct. In case of a correct login the number of login attempts is reset to 0, else increased by 1.
+     *  @param password The password provided for the login; this is to be compared to the password stored on the system.
+     */
+    public void login(String password) {
+        if(this.failedLoginAttempts < MAXIMALLOGINATTEMPTS) {
+            if(this.checkPassword(password)) {
+                this.setFailedLoginAttempts(0);
+                this.setLoggedIn(true);
+            }
+            else {
+                this.failedLoginAttempts++;
+                this.setLoggedIn(false);
+            }
+        }
+    }
+
+    /**
+     * Getter method to return the list of all titles bought by the user. 
+     * @return The current list of titles bought by the user.
+     */
+    public ArrayList<MusicTitle> getTitlesBought() {
+        return this.titlesBought;
+    }
+
+    /** 
+     * Adds the amount - if positive - to the current balance. 
+     * For non-positive amounts the action does nothing.
+     * @param amount The amount being deposited.
+     */
+    public void deposit(int amount) {
+        if(amount > 0) {
+            this.balance = this.balance + amount;
+        }
+    }
+
+    /**
+     *  Setter for the balance.
+     *  @param balance The new balance of the account.
+     */
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    /**
+     * Getter method to return the number of failed login attempts.
+     * @return The number of failed login attempts.
+     */
+    public int getFailedLoginAttempts() {
+        return this.failedLoginAttempts;
+    }
+
+    /**
+     *  Setter for the failed login attempts.
+     *  @param failedLoginAttempts The new number of failed login attempts.
+     */
+    public void setFailedLoginAttempts(int failedLoginAttempts) {
+        this.failedLoginAttempts = failedLoginAttempts;
+    }
+
+    /** 
+     *  If the user is logged in and has sufficient funds, a music title is bought by adding the title to the ArrayList of titlesBought of the customer. Furthermore the price of the title is subtracted from the funds of the customer. If the user is not logged in, a corresponding warning is to be printed. Likewise if the user has insufficient funds, they should be asked to pay money into their account.
+     *  @param musicTitle The music title that the customer wants to
+     *  buy.
+     */
+    public void buy(MusicTitle musicTitle) {
+
+    }
+
+    @Override
+    /**
+     * Standard toString method to represent the object in a human readable form. 
+     * If the user is not logged in, only a warning should be printed, but no substantial information be given.
+     * @return The object in a human readable form.
+     */
+    public String toString() {
+        if(this.getLoggedIn()) {
+            return "AccountStandard{" + this.getSalutation() + " " + this.getName() + ", \n You are logged in." + "}";
+        }
+        else {
+            return "No user log in!";
+        }
+    }
+}
