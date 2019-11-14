a19282a7cafbe4f9e23848ee93dc11856a657fa9
xiaoyaoyou95@hotmail.com <xiaoyaoyou95@hotmail.com>
2019-11-14 15:12:30 +0000

    Finished Self-Test for Ex2 of WS1-4 and Fix time

--- WS1-4/src/AircraftTests.java
+++ WS1-4/src/AircraftTests.java
@@ -11,117 +11,117 @@ import org.junit.jupiter.api.TestInfo;
  * <p>
  * This class contains 6 tests to test all the functions in class Aircraft and Aeroplane.
  * @author Zetian Qin zxq876
- * @version 2019-11-03 19:16:55
+ * @version 2019-11-14 14:24:28
  */
 public class AircraftTests {
-  public static final double TOLERANCE = 0.0000000001;
+    public static final double TOLERANCE = 0.0000000001;
 
-  private Aircraft aircraftTest1, aircraftTest2;
-  private Aeroplane aeroplaneTest;
+    private Aircraft aircraftTest1, aircraftTest2;
+    private Aeroplane aeroplaneTest;
 
-  @BeforeEach
-  public void setup(TestInfo testInfo) {
-    System.out.println("Start......" + testInfo.getDisplayName());
+    @BeforeEach
+    public void setup(TestInfo testInfo) {
+        System.out.println("Start......" + testInfo.getDisplayName());
 
-    //Test for class Aircraft
-    aircraftTest1 = new Aircraft(200, 900);
-    //Test for class Aerocraft
-    aeroplaneTest = new Aeroplane(300, 800, 20);
-    //Test for Polymorphism 
-    aircraftTest2 = new Aeroplane(350, 850, 19.9);
-  }
+        //Test for class Aircraft
+        aircraftTest1 = new Aircraft(200, 900);
+        //Test for class Aerocraft
+        aeroplaneTest = new Aeroplane(300, 800, 20);
+        //Test for Polymorphism 
+        aircraftTest2 = new Aeroplane(350, 850, 19.9);
+    }
 
-  @Test
-  @DisplayName("Getter Tests in class Aircraft")
-  public void test1() {
-    int expectedNumOfPassengers = 200;
-    int actualNumOfPassengers = aircraftTest1.getPassengerNumber();
-    assertEquals(expectedNumOfPassengers, actualNumOfPassengers);
+    @Test
+    @DisplayName("Getter Tests in class Aircraft")
+    public void test1() {
+        int expectedNumOfPassengers = 200;
+        int actualNumOfPassengers = aircraftTest1.getPassengerNumber();
+        assertEquals(expectedNumOfPassengers, actualNumOfPassengers);
 
-    int expectedMaxSpeed = 900;
-    int actualMaxSpeed = aircraftTest1.getMaxSpeed();
-    assertEquals(expectedMaxSpeed, actualMaxSpeed);
-  }
+        int expectedMaxSpeed = 900;
+        int actualMaxSpeed = aircraftTest1.getMaxSpeed();
+        assertEquals(expectedMaxSpeed, actualMaxSpeed);
+    }
 
-  @Test
-  @DisplayName("Setter Tests in class Aircraft")
-  public void test2() {
-    aircraftTest1.setPassengerNumber(322);
-    int expectedNumOfPassengers = 322;
-    int actualNumOfPassengers = aircraftTest1.getPassengerNumber();
-    assertEquals(expectedNumOfPassengers, actualNumOfPassengers);
+    @Test
+    @DisplayName("Setter Tests in class Aircraft")
+    public void test2() {
+        aircraftTest1.setPassengerNumber(322);
+        int expectedNumOfPassengers = 322;
+        int actualNumOfPassengers = aircraftTest1.getPassengerNumber();
+        assertEquals(expectedNumOfPassengers, actualNumOfPassengers);
 
-    aircraftTest1.setMaxSpeed(888);
-    int expectedMaxSpeed = 888;
-    int actualMaxSpeed = aircraftTest1.getMaxSpeed();
-    assertEquals(expectedMaxSpeed, actualMaxSpeed);
-  }
+        aircraftTest1.setMaxSpeed(888);
+        int expectedMaxSpeed = 888;
+        int actualMaxSpeed = aircraftTest1.getMaxSpeed();
+        assertEquals(expectedMaxSpeed, actualMaxSpeed);
+    }
 
-  @Test
-  @DisplayName("Getter Tests in class Aeroplane")
-  public void test3() {
-    int expectedNumOfPassengers = 300;
-    int actualNumOfPassengers = aeroplaneTest.getPassengerNumber();
-    assertEquals(expectedNumOfPassengers, actualNumOfPassengers);
+    @Test
+    @DisplayName("Getter Tests in class Aeroplane")
+    public void test3() {
+        int expectedNumOfPassengers = 300;
+        int actualNumOfPassengers = aeroplaneTest.getPassengerNumber();
+        assertEquals(expectedNumOfPassengers, actualNumOfPassengers);
 
-    int expectedMaxSpeed = 800;
-    int actualMaxSpeed = aeroplaneTest.getMaxSpeed();
-    assertEquals(expectedMaxSpeed, actualMaxSpeed);
+        int expectedMaxSpeed = 800;
+        int actualMaxSpeed = aeroplaneTest.getMaxSpeed();
+        assertEquals(expectedMaxSpeed, actualMaxSpeed);
 
-    double expectedFuelCons = 20;
-    double actualFuelCons = aeroplaneTest.getFuelConsumption();
-    assertEquals(expectedFuelCons, actualFuelCons, TOLERANCE);
-  }
+        double expectedFuelCons = 20;
+        double actualFuelCons = aeroplaneTest.getFuelConsumption();
+        assertEquals(expectedFuelCons, actualFuelCons, TOLERANCE);
+    }
 
-  @Test
-  @DisplayName("Setter Tests in class Aeroplane")
-  public void test4() {
-    aeroplaneTest.setPassengerNumber(333);
-    int expectedNumOfPassengers = 333;
-    int actualNumOfPassengers = aeroplaneTest.getPassengerNumber();
-    assertEquals(expectedNumOfPassengers, actualNumOfPassengers);
+    @Test
+    @DisplayName("Setter Tests in class Aeroplane")
+    public void test4() {
+        aeroplaneTest.setPassengerNumber(333);
+        int expectedNumOfPassengers = 333;
+        int actualNumOfPassengers = aeroplaneTest.getPassengerNumber();
+        assertEquals(expectedNumOfPassengers, actualNumOfPassengers);
 
-    aeroplaneTest.setMaxSpeed(777);
-    int expectedMaxSpeed = 777;
-    int actualMaxSpeed = aeroplaneTest.getMaxSpeed();
-    assertEquals(expectedMaxSpeed, actualMaxSpeed);
+        aeroplaneTest.setMaxSpeed(777);
+        int expectedMaxSpeed = 777;
+        int actualMaxSpeed = aeroplaneTest.getMaxSpeed();
+        assertEquals(expectedMaxSpeed, actualMaxSpeed);
 
 
-    aeroplaneTest.setFuelConsumption(11.11);
-    double expectedFuelCons = 11.11;
-    double actualFuelCons = aeroplaneTest.getFuelConsumption();
-    assertEquals(expectedFuelCons, actualFuelCons, TOLERANCE);
-  }
+        aeroplaneTest.setFuelConsumption(11.11);
+        double expectedFuelCons = 11.11;
+        double actualFuelCons = aeroplaneTest.getFuelConsumption();
+        assertEquals(expectedFuelCons, actualFuelCons, TOLERANCE);
+    }
 
-  @Test
-  @DisplayName("Getter Tests for Polymorphism")
-  public void test5() {
+    @Test
+    @DisplayName("Getter Tests for Polymorphism")
+    public void test5() {
 
-    int expectedNumOfPassengers = 350;
-    int actualNumOfPassengers = aircraftTest2.getPassengerNumber();
-    assertEquals(expectedNumOfPassengers, actualNumOfPassengers);
+        int expectedNumOfPassengers = 350;
+        int actualNumOfPassengers = aircraftTest2.getPassengerNumber();
+        assertEquals(expectedNumOfPassengers, actualNumOfPassengers);
 
-    int expectedMaxSpeed = 850;
-    int actualMaxSpeed = aircraftTest2.getMaxSpeed();
-    assertEquals(expectedMaxSpeed, actualMaxSpeed);
-  }
+        int expectedMaxSpeed = 850;
+        int actualMaxSpeed = aircraftTest2.getMaxSpeed();
+        assertEquals(expectedMaxSpeed, actualMaxSpeed);
+    }
 
-  @Test
-  @DisplayName("Setter Tests for Polymorphism")
-  public void test6() {
+    @Test
+    @DisplayName("Setter Tests for Polymorphism")
+    public void test6() {
 
-    aircraftTest2.setPassengerNumber(222);
-    int expectedNumOfPassengers = 222;
-    int actualNumOfPassengers = aircraftTest2.getPassengerNumber();
-    assertEquals(expectedNumOfPassengers, actualNumOfPassengers);
+        aircraftTest2.setPassengerNumber(222);
+        int expectedNumOfPassengers = 222;
+        int actualNumOfPassengers = aircraftTest2.getPassengerNumber();
+        assertEquals(expectedNumOfPassengers, actualNumOfPassengers);
 
-    aircraftTest2.setMaxSpeed(888);
-    int expectedMaxSpeed = 888;
-    int actualMaxSpeed = aircraftTest2.getMaxSpeed();
-    assertEquals(expectedMaxSpeed, actualMaxSpeed);
-  }
+        aircraftTest2.setMaxSpeed(888);
+        int expectedMaxSpeed = 888;
+        int actualMaxSpeed = aircraftTest2.getMaxSpeed();
+        assertEquals(expectedMaxSpeed, actualMaxSpeed);
+    }
 
-  @AfterEach
+    @AfterEach
 	public void tearDown(TestInfo testInfo) {
 		System.out.println("Finished..." + testInfo.getDisplayName());
 	}
--- WS1-4/src/SubscriptionTests.java
+++ WS1-4/src/SubscriptionTests.java
@@ -1,115 +1,89 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 
 /**
- * Public tests for Exercise 2, Worksheet 4.
- * 
- * @author Alexandros Evangelidis
- * @version 2019-11-11
+ * In Self-Testing, the code should be tested against three different types, normal cases, boundary cases, and illegal cases.
+ * <p>
+ * This class contains 3 tests to test all the functions in class Subscription and SubscriptionPrime.
+ * @author Zetian Qin zxq876
+ * @version 2019-11-14 14:24:34
  */
 public class SubscriptionTests {
+    private SubscriptionInterface subInterfaceTest;
+    private SubscriptionPrime subPrimeTest;
+    private Subscription subTest;
 
-  private SubscriptionInterface sub1;
-  private SubscriptionPrime subPrime1;
-  private Subscription sub2;
+    @BeforeEach
+    public void setup(TestInfo testInfo) {
+        System.out.println("Start......" + testInfo.getDisplayName());
 
-  @BeforeEach
-  public void init() {
-    sub1 = new Subscription("Financial Times", "mary@mary.org", 2);
-    subPrime1 = new SubscriptionPrime("Garden News", "josh@josh.org", 3, "CS Birmingham");
-    sub2 = new SubscriptionPrime("Athletic News", "jack@jack.org", 4, "Bristol street 15092");
-  }
+        //Test for Subscription
+        subInterfaceTest = new Subscription("Journal Test1", "subInterfaceTest@test.com", 1);
+        //Test for class SubscriptionPrime
+        subPrimeTest = new SubscriptionPrime("Journal Test2", "subPrimeTest@test.com", 3, "Address Test2");
+        //Test for Polymorphism 
+        subTest = new SubscriptionPrime("Journal Test3", "subTest@test.com", 2, "Address Test3");
+    }
 
-  // testing sub1's getTitle()
-  @Test
-  public void test1() {
-    String expectedTitle = "Financial Times";
-    String actualTitle = sub1.getTitle();
-    assertEquals(expectedTitle, actualTitle, "title should be: " + actualTitle);
-  }
+    @Test
+    @DisplayName("Getter Tests for Subscription")
+    public void test1() {
+        String expectedTitle = "Journal Test1";
+        String actualTitle = subInterfaceTest.getTitle();
+        assertEquals(expectedTitle, actualTitle, "The correct Title should be: " + expectedTitle);
 
-  // testing sub1's getEmail()
-  @Test
-  public void test2() {
-    String expectedEmail = "mary@mary.org";
-    String actualEmail = sub1.getEmail();
-    assertEquals(expectedEmail, actualEmail, "mail should be: " + actualEmail);
-  }
+        String expectedEmail = "subInterfaceTest@test.com";
+        String actualEmail = subInterfaceTest.getEmail();
+        assertEquals(expectedEmail, actualEmail, "The correct E-mail address should be: " + expectedEmail);
 
-  // testing sub1's getCost()
-  @Test
-  public void test3() {
-    int expectedCost = 2;
-    int actualCost = sub1.getCost();
-    assertEquals(expectedCost, actualCost, "cost should be: " + actualCost);
-  }
+        int expectedCost = 1;
+        int actualCost = subInterfaceTest.getCost();
+        assertEquals(expectedCost, actualCost, "The correct Cost of Journal should be: " + expectedCost);
+    }
 
-  // testing subPrime1's getTitle()
-  @Test
-  public void test4() {
-    String expectedTitle = "Garden News";
-    String actualTitle = subPrime1.getTitle();
-    assertEquals(expectedTitle, actualTitle, "title should be: " + actualTitle);
-  }
+    @Test
+    @DisplayName("Getter Tests for SubscriptionPrime")
+    public void test2() {
+        String expectedTitle = "Journal Test2";
+        String actualTitle = subPrimeTest.getTitle();
+        assertEquals(expectedTitle, actualTitle, "The correct Title should be: " + expectedTitle);
 
-  // testing subPrime1's getEmail()
-  @Test
-  public void test5() {
-    String expectedEmail = "josh@josh.org";
-    String actualEmail = subPrime1.getEmail();
-    assertEquals(expectedEmail, actualEmail, "mail should be: " + actualEmail);
+        String expectedEmail = "subPrimeTest@test.com";
+        String actualEmail = subPrimeTest.getEmail();
+        assertEquals(expectedEmail, actualEmail, "The correct E-mail address should be: " + expectedEmail);
 
-  }
+        int expectedCost = 3;
+        int actualCost = subPrimeTest.getCost();
+        assertEquals(expectedCost, actualCost, "The correct Cost of Journal should be: " + expectedCost);
 
-  // testing subPrime1's getCost()
-  @Test
-  public void test6() {
-    int expectedCost = 3;
-    int actualCost = subPrime1.getCost();
-    assertEquals(expectedCost, actualCost, "cost should be: " + actualCost);
-  }
+        String expectedDeliveryAddress = "Address Test2";
+        String actualDeliveryAddress = subPrimeTest.getAddress();
+        assertEquals(expectedDeliveryAddress, actualDeliveryAddress, "The correct Delivery Address should be: " + expectedDeliveryAddress);
+    }
 
-  // testing subPrime1's getAddress()
-  @Test
-  public void test7() {
-    String expectedDeliveryAddress = "CS Birmingham";
-    String actualDeliveryAddress = subPrime1.getAddress();
-    assertEquals(expectedDeliveryAddress, actualDeliveryAddress,
-        "address should be: " + actualDeliveryAddress);
-  }
+    @Test
+    @DisplayName("Getter Tests for Polymorphism")
+    public void test3() {
+        String expectedTitle = "Journal Test3";
+        String actualTitle = subTest.getTitle();
+        assertEquals(expectedTitle, actualTitle, "The correct Title should be: " + expectedTitle);
 
-  // testing getAddress()
-  @Test
-  public void test8() {
-    SubscriptionPrimeInterface subPrime2 =
-        new SubscriptionPrime("Garden News", "josh@josh.org", 3, "New Street 5005");
-    String expectedAddress = "New Street 5005";
-    String actualAddress = subPrime2.getAddress();
-    assertEquals(expectedAddress, actualAddress, "address should be: " + actualAddress);
-  }
+        String expectedEmail = "subTest@test.com";
+        String actualEmail = subTest.getEmail();
+        assertEquals(expectedEmail, actualEmail, "The correct E-mail address should be: " + expectedEmail);
 
-  // testing sub2's getTitle
-  @Test
-  public void test9() {
-    String expectedTitle = "Athletic News";
-    String actualTitle = sub2.getTitle();
-    assertEquals(expectedTitle, actualTitle, "title should be: " + actualTitle);
-  }
+        int expectedCost = 2;
+        int actualCost = subTest.getCost();
+        assertEquals(expectedCost, actualCost, "The correct Cost of Journal should be: " + actualCost);
+    }
 
-  // testing sub2's getEmail()
-  @Test
-  public void test10() {
-    String expectedEmail = "jack@jack.org";
-    String actualEmail = sub2.getEmail();
-    assertEquals(expectedEmail, actualEmail, "email should be: " + actualEmail);
-  }
-
-  // testing sub2's getCost()
-  @Test
-  public void test11() {
-    int expectedCost = 4;
-    int actualCost = sub2.getCost();
-    assertEquals(expectedCost, actualCost, "cost should be: " + actualCost);
-  }
+    @AfterEach
+	public void tearDown(TestInfo testInfo) {
+		System.out.println("Finished..." + testInfo.getDisplayName());
+	}
 }
