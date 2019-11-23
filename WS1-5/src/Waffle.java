23a91fb4de6c7e98467ce9a9f39a7b6c08888f39
xiaoyaoyou95@hotmail.com <xiaoyaoyou95@hotmail.com>
2019-11-23 18:12:51 +0000

    Compute the number of square of each item Finished.

--- WS1-5/src/Waffle.java
+++ WS1-5/src/Waffle.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Waffle extends Application {
+    private int maximum;
+    private static final int MAX_NUMBER_OF_SQUARE  = 100;
     
     @Override
     public void start(Stage arg0) throws Exception {
@@ -24,6 +27,22 @@ public class Waffle extends Application {
         //Sort Array expenditures
         Arrays.sort(expenditures, (Expenditure exp1, Expenditure exp2) -> exp2.getValue() - exp1.getValue());
 
-        
+        //Computer number of square of each item.
+        double sum = 0;
+        for(Expenditure itor:expenditures) {
+            sum += itor.getValue();
+        }
+        ArrayList<Integer> numberOfEachItem = new ArrayList<Integer>();
+        int sumOfEachItem = 0;
+        for(Expenditure itor:expenditures) {
+            int numberOfEach = (int)Math.round(itor.getValue() / sum);
+            sumOfEachItem += numberOfEach;
+            if(sumOfEachItem <= MAX_NUMBER_OF_SQUARE) {
+                numberOfEachItem.add(numberOfEach);
+            }
+            else {
+                numberOfEach = MAX_NUMBER_OF_SQUARE - sumOfEachItem;
+            }
+        }
     }
 }
