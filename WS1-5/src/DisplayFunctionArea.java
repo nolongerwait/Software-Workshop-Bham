ad381f9b9ec31ec3fd2e0d4c0ae3b5b73137d1a1
xiaoyaoyou95@hotmail.com <xiaoyaoyou95@hotmail.com>
2019-11-28 17:17:39 +0000

    Finished the Ex4.

--- WS1-5/src/DisplayFunctionArea.java
+++ WS1-5/src/DisplayFunctionArea.java
@@ -9,10 +9,10 @@ import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Polyline;
 
 /**
- *  This class is to display a non-constant function such as x goes to x*x and the area between the function graph and the x-axis in a given interval [a,b].  It is done by drawing the x-axis and the y-axis as well as defining a polyline from the function, the end-point (b, f(b)) and the starting point at (a, f(a)) and then displaying the corresponding polygon filled with colour and adquately scaled.
+ * This class is to display a non-constant function such as x goes to x*x and the area between the function graph and the x-axis in a given interval [a,b].  It is done by drawing the x-axis and the y-axis as well as defining a polyline from the function, the end-point (b, f(b)) and the starting point at (a, f(a)) and then displaying the corresponding polygon filled with colour and adquately scaled.
  *
- *  @version 2019-11-17
- *  @author Manfred Kerber
+ * @version 2019-11-17
+ * @author Zetian Qin zxq876 and Manfred Kerber
  */
 public class DisplayFunctionArea extends Application{
 
@@ -25,7 +25,7 @@ public class DisplayFunctionArea extends Application{
     public static final int Y_SIZE = 600;
 
     /**
-     *  f is the function to be displayed. We introduce it as a global variable so that it can be used in the start method, but be defined in the main method.
+     * f is the function to be displayed. We introduce it as a global variable so that it can be used in the start method, but be defined in the main method.
      */
     private static Function<Double,Double> f;
 
@@ -37,7 +37,7 @@ public class DisplayFunctionArea extends Application{
     /**
       * b is the right border of the interval on which the function is to be displayed. We introduce it as a global variable so that it can be used in the start method, but be defined in the main method.
       */
-   private static double b;
+    private static double b;
 
     /** min is an approximation of the minimum of f in the interval [a,b]. It is introduced as a global variable so that it does not have to be recomputed.
      */
@@ -69,12 +69,12 @@ public class DisplayFunctionArea extends Application{
 
 
     /**
-     *   The method generates a polygon corresponding to the area between the function f and the x-axis in the interval [a,b].
-     *   @param f The function to be drawn.
-     *   @param n The number of equidistant intervals to be drawn.
-     *   @param a The minimal x-value in the interval. 
-     *   @param b The maximal x-value in the interval. 
-     *   @return The polygon corresponding to the area between the function f and the x-axis over the interval [a,b] with granularity n.
+     * The method generates a polygon corresponding to the area between the function f and the x-axis in the interval [a,b].
+     * @param f The function to be drawn.
+     * @param n The number of equidistant intervals to be drawn.
+     * @param a The minimal x-value in the interval. 
+     * @param b The maximal x-value in the interval. 
+     * @return The polygon corresponding to the area between the function f and the x-axis over the interval [a,b] with granularity n.
      */
     public static Polygon functionToPolygon(Function<Double,Double> f,
                                     int n, double a, double b) {
@@ -90,38 +90,64 @@ public class DisplayFunctionArea extends Application{
             for (int i = 0; i <= n ; i++){
                 x = a + (b - a) * i / n;
                 y = f.apply(x);
-                points[2*i]   = x;
-                points[2*i+1] = y;
+                points[2*i]   = (x - a) * X_SIZE / (b - a);
+                points[2*i+1] = (max - y) * Y_SIZE / (max - min);
             }
+            // Changed - The last two Point of the Polygon have not been added in, so I add these now.
+            // Please note this, the Fisrt one must be the the Right endpoint of the X axis.
+            points[2 * (n + 1)] = X_SIZE;
+            points[2 * (n + 1) + 1] = (max - 0) * Y_SIZE / (max - min);
+            // And the Second one is the Lift endpoint of the X axis.
+            points[2 * (n + 1) + 2] = 0;
+            points[2 * (n + 1) + 3] = (max - 0) * Y_SIZE / (max - min);
             /* Draw graph */
             return new Polygon(points);
         }
     }
 
     /**
-     *   The method generates a polyline corresponding to the graph of function f in the interval [a,b]
-     *   @param f The function to be drawn.
-     *   @param n The number of equidistant intervals to be drawn.
-     *   @param a The minimal x-value in the interval. 
-     *   @param b The maximal x-value in the interval. 
-     *   @return The polyline corresponding to the function f over the interval [a,b] with granularity n.
+     * The method generates a polyline corresponding to the graph of function f in the interval [a,b]
+     * @param f The function to be drawn.
+     * @param n The number of equidistant intervals to be drawn.
+     * @param a The minimal x-value in the interval. 
+     * @param b The maximal x-value in the interval. 
+     * @return The polyline corresponding to the function f over the interval [a,b] with granularity n.
      */
-    public static Polyline functionToPolyline(Function<Double,Double> f,
-                                    int n, double a, double b) {
+    public static Polyline functionToPolyline(Function<Double,Double> f, int n, double a, double b) {
         if (a >= b) {
             throw new IllegalArgumentException();
         } else {
             double[] points = new double[2*(n+1)];
             double x, y;
+            // Changed - Using the following loop, compute the min and max of f in the interval [a,b].
+            double minTmp = f.apply(a);
+            double maxTmp = f.apply(a);
+
             /* Loop: Add x and y values to the corresponding arrays for a, b and n-1 equidistant values in between.
              * The (x_i,y_i) values are added to the points array in pairs.
              */
-            for (int i = 0; i <= n ; i++){
+            for(int i = 0; i <= n; i++) {
                 x = a + (b - a) * i / n;
                 y = f.apply(x);
-                points[2*i]   = x;
-                points[2*i+1] = y;
+                // Changed - The x is not the X coordinate of Point in Polyline, so I need to compute the right X coordinate.
+                points[2*i]   = (x - a) * X_SIZE / (b - a);
+                points[2*i+1] = y; // I do not fix this bug since I have not got the value of min and max.
+
+                if(minTmp > y) {
+                    minTmp = y;
+                }
+                if(maxTmp < y) {
+                    maxTmp = y;
+                }
             }
+
+            min = minTmp;
+            max = maxTmp;
+            // Changed - Fine, I got the min and max, then, I could compute the right Y coordinate of Point in Polyline.
+            for(int i = 0; i <= n; i++) {
+                points[2 * i + 1] = (max - points[2 * i + 1]) * Y_SIZE / (max - min);
+            }
+
             /* Draw graph */
             Polyline result = new Polyline(points);
             result.setStrokeWidth(3);
@@ -130,9 +156,9 @@ public class DisplayFunctionArea extends Application{
     }
 
     /**
-     *   The method draws the x-axis if 0 is in the interval ]min,max[.
-     *   It is assumed that the function is not constant.
-     *   @param root The group to which the x-axis is to be added.
+     * The method draws the x-axis if 0 is in the interval [min, max].
+     * It is assumed that the function is not constant.
+     * @param root The group to which the x-axis is to be added.
      */
     public static void drawXAxis(Group root){
         if (min < 0 && 0 < max) {
@@ -144,13 +170,14 @@ public class DisplayFunctionArea extends Application{
     }
 
     /**
-     *   The method draws the y-axis if 0 is in the interval ]a, b[.
-     *   @param root The group to which the y-axis is to be added.
+     * The method draws the y-axis if 0 is in the interval [a, b].
+     * @param root The group to which the y-axis is to be added.
      */
     public static void drawYAxis(Group root){
         if (a < 0 && 0 < b) {
-            Line line = new Line((b-0) * X_SIZE/(b-a), 0,
-                                 (b-0) * X_SIZE/(b-a), Y_SIZE);
+            // Changed - The abscissa of the Y axis should be 0 - a instead of b - 0.
+            Line line = new Line((0 - a) * X_SIZE / (b - a), 0,
+                                 (0 - a) * X_SIZE / (b - a), Y_SIZE);
             line.setStrokeWidth(2);
             root.getChildren().add(line);
         }
@@ -158,7 +185,7 @@ public class DisplayFunctionArea extends Application{
     
 
     /**
-     *  @param stage The window to be displayed.
+     * @param stage The window to be displayed.
      */
     @Override
     public void start(Stage stage) throws Exception {
@@ -180,20 +207,20 @@ public class DisplayFunctionArea extends Application{
 
     
     /**
-     *  The method gives values to the static variables a, b, n, and polyline.
-     *  @param f The function to be displayed.
-     *  @param numberOfValues The number of points on the polyline.
-     *  @param left The left border of the interval to be displayed.
-     *  @param right The right border of the interval to be displayed.
+     * The method gives values to the static variables a, b, n, and polyline.
+     * @param f The function to be displayed.
+     * @param numberOfValues The number of points on the polyline.
+     * @param left The left border of the interval to be displayed.
+     * @param right The right border of the interval to be displayed.
      */
     public static void displayFunctionArea(Function<Double, Double> f, double left, double right, int numberOfValues){
-            a = left;
-            b = right;
-            n = numberOfValues;
-            polyline = functionToPolyline(f, numberOfValues, left, right);
-            polygon = functionToPolygon(f, numberOfValues, left, right);
-            polygon.setFill(areaColour);
-        }
+        a = left;
+        b = right;
+        n = numberOfValues;
+        polyline = functionToPolyline(f, numberOfValues, left, right);
+        polygon = functionToPolygon(f, numberOfValues, left, right);
+        polygon.setFill(areaColour);
+    }
         
     /*
      *  main method to launch the application.
