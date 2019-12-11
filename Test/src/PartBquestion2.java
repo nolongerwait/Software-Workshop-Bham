113de19538a29c5c7c6a423cce4de203cb7fa412
xiaoyaoyou95@hotmail.com <xiaoyaoyou95@hotmail.com>
2019-12-11 19:35:37 +0000

    Continue to optimize

--- Test/src/PartBquestion2.java
+++ Test/src/PartBquestion2.java
@@ -3,12 +3,19 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PartBquestion2 {
+/**
+ * This program designed for the Question 2 of Part B in Assignment 3 Algorithm Design & Analysis (and Revision).
+ * <pre>
+ * </pre>
+ * @author Zetian Qin zxq876
+ * @version 2019-12-11 16:58:09
+ */
+public class PartBQuestion2 {
 
     /**
      * HasDigit method could check whether the String contains digit or not.
      * @param content The string which would be checked, as String.
-     * @return Whether the String contains digit or not, true - if the string contains digits, false - if the string does not contain digits, as boolean.
+     * @return Whether the String contains digit or not, [true - if the string contains digits, false - if the string does not contain digits], as boolean.
      */
     public static boolean HasDigit(String content) {
         boolean flag = false;
@@ -28,13 +35,49 @@ public class PartBquestion2 {
     public static void subStatements(String str, Map<Character,Integer> storeVariables) {
         String strCopy = str.replaceAll(" ", "");
         if(strCopy.contains("+")) {
-            if(HasDigit(strCopy)) {
-                String number1 = strCopy.substring(strCopy.indexOf("=") + 1, strCopy.indexOf("+") - 1);
-                String number2 = strCopy.substring(strCopy.indexOf("+") + 1, strCopy.length());
-                storeVariables.put(strCopy.charAt(0), Integer.parseInt(number1) + Integer.parseInt(number2));
+            String addend1 = strCopy.substring(strCopy.indexOf("=") + 1, strCopy.indexOf("+"));
+            String addend2 = strCopy.substring(strCopy.indexOf("+") + 1, strCopy.length());
+            // Both two addends have digits.
+            if(HasDigit(addend1)&&HasDigit(addend2)) {
+                storeVariables.put(strCopy.charAt(0), Integer.parseInt(addend1) + Integer.parseInt(addend2));
+            }
+            // Only addend1 has digits.
+            else if(HasDigit(addend1)) {
+                // Check if there is a negative sign before the variable(addend2).
+                if(addend2.contains("-")) {
+                    storeVariables.put(strCopy.charAt(0), Integer.parseInt(addend1) - storeVariables.get(addend2.charAt(1)));
+                }
+                else {
+                    storeVariables.put(strCopy.charAt(0), Integer.parseInt(addend1) + storeVariables.get(addend2.charAt(0)));
+                }
+            }
+            // Only addend2 has digits.
+            else if(HasDigit(addend2)) {
+                // Check if there is a negative sign before the variable(addend1).
+                if(addend1.contains("-")) {
+                    storeVariables.put(strCopy.charAt(0), Integer.parseInt(addend2) - storeVariables.get(addend1.charAt(1)));
+                }
+                else {
+                    storeVariables.put(strCopy.charAt(0), Integer.parseInt(addend2) + storeVariables.get(addend1.charAt(0)));
+                }
             }
             else {
-                storeVariables.put(strCopy.charAt(0),storeVariables.get(strCopy.charAt(2)) + storeVariables.get(strCopy.charAt(4)));
+                // Check if there are negative signs before both two addends.
+                if(addend1.contains("-") && addend2.contains("-")) {
+                    storeVariables.put(strCopy.charAt(0), -(storeVariables.get(addend1.charAt(1))) - storeVariables.get(addend2.charAt(1)));
+                }
+                // Only one negative sign before addend1.
+                else if(addend1.contains("-")) {
+                    storeVariables.put(strCopy.charAt(0), -(storeVariables.get(addend1.charAt(1))) + storeVariables.get(addend2.charAt(0)));
+                }
+                // Only one negative sign before addend2.
+                else if(addend2.contains("-")) {
+                    storeVariables.put(strCopy.charAt(0), storeVariables.get(addend1.charAt(0)) - storeVariables.get(addend2.charAt(1)));
+                }
+                // No negative sign before both addends.
+                else {
+                    storeVariables.put(strCopy.charAt(0), storeVariables.get(addend1.charAt(0)) + storeVariables.get(addend2.charAt(0)));
+                }
             }
         }
         else {
@@ -56,18 +99,23 @@ public class PartBquestion2 {
         Map<Character, Integer> storeVariables = new HashMap<Character, Integer>();
         String[] subStatement = str.split("\n");
         for(int i = 0; i < subStatement.length; i++) {
-            if(subStatement[i].length() == 1) {
-                result = storeVariables.get(subStatement[i].charAt(0));
+            if(subStatement[i].contains("=")) {
+                subStatements(subStatement[i], storeVariables);
             }
             else {
-                subStatements(subStatement[i], storeVariables);
+                if(subStatement[i].contains("-")) {
+                    result = -(storeVariables.get(subStatement[i].charAt(1)));
+                }
+                else {
+                    result = storeVariables.get(subStatement[i].charAt(0));
+                }
             }
         }
         return result;
     }
 
     public static void main(String[] args) {
-        String str = "A = 2\nB = 8\nC = A + B\nC\n";
+        String str = "A = 2\nB = 22\nK = 91\nK = -A + -1000000\nZ = -K + A\nZ\n";
         int result = computeTheResult(str);
         System.out.println(result);
     }
