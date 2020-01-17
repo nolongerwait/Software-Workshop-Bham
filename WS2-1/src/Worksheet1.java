fb012b7256157cdb0c3359f754daf4df74799be6
xiaoyaoyou95@hotmail.com <xiaoyaoyou95@hotmail.com>
2020-01-17 20:15:21 +0000

    Finished the WorkSheet 2-1

--- WS2-1/src/Worksheet1.java
+++ WS2-1/src/Worksheet1.java
@@ -10,7 +10,7 @@ public class Worksheet1 {
 
 	static int power(int m, int n) {
 		if(m == 0 && n == 0) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("O to the power of 0 is undefined!");
 		}
 		else if(n == 0) {
 			return 1;
@@ -22,7 +22,7 @@ public class Worksheet1 {
 
 	static int fastPower(int m, int n) {
 		if(m == 0 && n == 0) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("O to the power of 0 is undefined!");
 		}
 		else if(n == 0) {
 			return 1;
@@ -38,43 +38,103 @@ public class Worksheet1 {
 	// Exercise 2
 
 	static List<Integer> negateAll(List<Integer> a) {
-		return new List<Integer>(); // replace this by your code
+		if(a.isEmpty()) {
+			return new List<Integer>();
+		}
+		else {
+			return new List<Integer>(0 - a.getHead(), negateAll(a.getTail()));
+		}
 	}
 
 	// Exercise 3
 
 	static int find(int x, List<Integer> a) {
-		return 0; // replace this by your code
+		if(a.isEmpty()) {
+			throw new IllegalArgumentException(x + "does not appear in the list!");
+		}
+		else if(a.getHead() == x) {
+			return 0;
+		}
+		else {
+			return 1 + find(x, a.getTail());
+		}
 	}
 
 	// Exercise 4
 
 	static boolean allEven(List<Integer> a) {
-		return true; // replace this by your code
+		if(a.isEmpty()) {
+			return true;
+		}
+		else if(a.getHead() % 2 == 0) {
+			return true && allEven(a.getTail());
+		}
+		else {
+			return false;
+		}
 	}
 
 	// Exercise 5
 
 	static List<Integer> evenNumbers(List<Integer> a) {
-		return new List<Integer>(); // replace this by your code
+		if(a.isEmpty()) {
+			return new List<Integer>();
+		}
+		else if(a.getHead() % 2 == 0) {
+			return new List<Integer>(a.getHead(), evenNumbers(a.getTail()));
+		}
+		else {
+			return evenNumbers(a.getTail());
+		}
 	}
 
 	// Exercise 6
 
 	static boolean sorted(List<Integer> a) {
-		return true; // replace this by your code
+		if(a.isEmpty() || a.getTail().isEmpty()) {
+			return true;
+		}
+		else if(a.getHead() > a.getTail().getHead()) {
+			return true && sorted(a.getTail());
+		}
+		else {
+			return false;
+		}
 	}
 
 	// Exercise 7
 
 	static List<Integer> merge(List<Integer> a, List<Integer> b) {
-		return new List<Integer>(); // replace this by your code
+		if(a.isEmpty()) {
+			return b;
+		}
+		if(b.isEmpty()) {
+			return a;
+		}
+
+		if(a.getHead() >= b.getHead()) {
+			return new List<Integer>(a.getHead(), merge(a.getTail(), b));
+		}
+		else {
+			return new List<Integer>(b.getHead(), merge(a, b.getTail()));
+		}
 	}
 
 	// Exercise 8
 
 	static List<Integer> removeDuplicates(List<Integer> a) {
-		return new List<Integer>(); // replace this by your code
+		if(a.isEmpty()) {
+			return new List<Integer>();
+		}
+		else if(a.getTail().isEmpty()) {
+			return new List<Integer>(a.getHead(), new List<Integer>());
+		}
+		else if(a.getHead() == a.getTail().getHead()) {
+			return removeDuplicates(a.getTail());
+		}
+		else {
+			return new List<Integer>(a.getHead(), removeDuplicates(a.getTail()));
+		}
+		
 	}
-
 }
--- WS2-1/src/Worksheet1Test.java
+++ WS2-1/src/Worksheet1Test.java
@@ -11,29 +11,172 @@ import org.junit.jupiter.api.*;
  */
 
 public class Worksheet1Test {
-    private Worksheet1 worksheet1test;
 
     @BeforeEach
 	public void setup(TestInfo testInfo) {
         System.out.println("Start......" + testInfo.getDisplayName());
-        worksheet1test = new Worksheet1();
     }
     
     @Test
     @DisplayName("Tests for the Exercise 1")
     public void test1() {
-        assertEquals(worksheet1test.power(3,2), 9);
-        assertEquals(worksheet1test.power(2,10), 1024);
-        assertEquals(worksheet1test.power(4,0), 1);
-        assertEquals(worksheet1test.power(5,1), 5);
+        // Tests for power() method
+        assertEquals(9, Worksheet1.power(3,2));
+        assertEquals(1024, Worksheet1.power(2,10));
+        assertEquals(1, Worksheet1.power(4,0));
+        assertEquals(5, Worksheet1.power(5,1));
+        assertThrows(IllegalArgumentException.class, () -> Worksheet1.power(0, 0), "Expected power() to throw, but it didn't");
 
-        assertEquals(worksheet1test.fastPower(2,10), 1024);
+        // Test for fastPower() method
+        assertEquals(9, Worksheet1.fastPower(3,2));
+        assertEquals(1024, Worksheet1.fastPower(2,10));
+        assertEquals(1, Worksheet1.fastPower(4,0));
+        assertEquals(5, Worksheet1.fastPower(5,1));
+        assertThrows(IllegalArgumentException.class, () -> Worksheet1.fastPower(0, 0), "Expected fastPower() to throw, but it didn't");
     }
 
     @Test
     @DisplayName("Tests for the Exercise 2")
     public void test2() {
-        
+        // Test for the List in normal case 
+        List<Integer> testCase1 = new List<Integer>(2,new List<Integer>(-5, new List<Integer>(8, new List<Integer>(0,new List<Integer>()))));
+
+        List<Integer> exceptedCase1 = new List<Integer>(-2, new List<Integer>(5, new List<Integer>(-8, new List<Integer>(0, new List<Integer>()))));
+
+        assertEquals(exceptedCase1, Worksheet1.negateAll(testCase1));
+
+        // Test for the empty List
+        List<Integer> testCase2 = new List<Integer>();
+
+        List<Integer> exceptedCase2 = new List<Integer>();
+
+        assertEquals(exceptedCase2, Worksheet1.negateAll(testCase2));
+    }
+
+    @Test
+    @DisplayName("Tests for the Exercise 3")
+    public void test3() {
+        List<Integer> testCase = new List<Integer>(7,new List<Integer>(5, new List<Integer>(3, new List<Integer>(8,new List<Integer>()))));
+
+        assertEquals(2, Worksheet1.find(3,testCase));
+        assertEquals(0, Worksheet1.find(7,testCase));
+        assertEquals(1, Worksheet1.find(5,testCase));
+        assertEquals(3, Worksheet1.find(8,testCase));
+        assertThrows(IllegalArgumentException.class, () -> Worksheet1.find(0, testCase), "Expected find() to throw, but it didn't");
+    }
+
+    @Test
+    @DisplayName("Tests for the Exercise 4")
+    public void test4() {
+        // Test for the normal case - False
+        List<Integer> testCase1 = new List<Integer>(2,new List<Integer>(3,new List<Integer>(-5, new List<Integer>(8, new List<Integer>(-2, new List<Integer>())))));
+
+        assertFalse(Worksheet1.allEven(testCase1));
+
+        // Test for the normal case - True
+        List<Integer> testCase2 = new List<Integer>(2,new List<Integer>(4,new List<Integer>(-6, new List<Integer>(8, new List<Integer>(-2, new List<Integer>())))));
+
+        assertTrue(Worksheet1.allEven(testCase2));
+    }
+
+    @Test
+    @DisplayName("Tests for the Exercise 5")
+    public void test5() {
+        // Test for the normal case
+        List<Integer> testCase1 = new List<Integer>(2,new List<Integer>(3,new List<Integer>(-5, new List<Integer>(8, new List<Integer>(-2, new List<Integer>())))));
+
+        List<Integer> exceptedCase1 = new List<Integer>(2, new List<Integer>(8, new List<Integer>(-2, new List<Integer>())));
+
+        assertEquals(exceptedCase1, Worksheet1.evenNumbers(testCase1));
+
+        // Test for the List does not contain even numbers
+        List<Integer> testCase2 = new List<Integer>(1,new List<Integer>(3,new List<Integer>(-5, new List<Integer>(9, new List<Integer>(-1, new List<Integer>())))));
+
+        List<Integer> exceptedCase2 = new List<Integer>();
+
+        assertEquals(exceptedCase2, Worksheet1.evenNumbers(testCase2));
+
+        // Test for empty List
+        List<Integer> testCase3 = new List<Integer>();
+
+        List<Integer> exceptedCase3 = new List<Integer>();
+
+        assertEquals(exceptedCase3, Worksheet1.evenNumbers(testCase3));
+    }
+
+    @Test
+    @DisplayName("Tests for the Exercise 6")
+    public void test6() {
+        // Test for the normal case - True
+        List<Integer> testCase1 = new List<Integer>(9,new List<Integer>(7,new List<Integer>(1, new List<Integer>(0, new List<Integer>(-2, new List<Integer>())))));
+
+        assertTrue(Worksheet1.sorted(testCase1));
+
+        // Test for the normal case - False
+        List<Integer> testCase2 = new List<Integer>(2,new List<Integer>(4,new List<Integer>(-6, new List<Integer>(8, new List<Integer>(-2, new List<Integer>())))));
+
+        assertFalse(Worksheet1.sorted(testCase2));
+    }
+
+    @Test
+    @DisplayName("Tests for the Exercise 7")
+    public void test7() {
+        // Test for the number of Node in List a is less than List b
+        List<Integer> testCase1a = new List<Integer>(8,new List<Integer>(2,new List<Integer>()));
+        List<Integer> testCase1b = new List<Integer>(9,new List<Integer>(8, new List<Integer>(7, new List<Integer>(5,new List<Integer>()))));
+
+        List<Integer> exceptedCase1 = new List<Integer>(9, new List<Integer>(8, new List<Integer>(8, new List<Integer>(7, new List<Integer>(5, new List<Integer>(2, new List<Integer>()))))));
+
+        assertEquals(exceptedCase1, Worksheet1.merge(testCase1a,testCase1b));
+
+        // Test for the number of Node in List a is more than List b
+        List<Integer> testCase2a = new List<Integer>(8,new List<Integer>(5,new List<Integer>(5, new List<Integer>(2, new List<Integer>()))));
+        List<Integer> testCase2b = new List<Integer>(9,new List<Integer>());
+
+        List<Integer> exceptedCase2 = new List<Integer>(9, new List<Integer>(8, new List<Integer>(5, new List<Integer>(5, new List<Integer>(2, new List<Integer>())))));
+
+        assertEquals(exceptedCase2, Worksheet1.merge(testCase2a,testCase2b));
+
+        // Test for the number of Node in List a is equal to List b
+        List<Integer> testCase3a = new List<Integer>(8,new List<Integer>(5,new List<Integer>(5, new List<Integer>(2, new List<Integer>()))));
+        List<Integer> testCase3b = new List<Integer>(9,new List<Integer>(8, new List<Integer>(7, new List<Integer>(5, new List<Integer>()))));
+        List<Integer> exceptedCase3 = new List<Integer>(9, new List<Integer>(8, new List<Integer>(8, new List<Integer>(7, new List<Integer>(5, new List<Integer>(5, new List<Integer>(5, new List<Integer>(2, new List<Integer>()))))))));
+
+        assertEquals(exceptedCase3, Worksheet1.merge(testCase3a,testCase3b));
+
+        // Test for the List a is empty
+        List<Integer> testCase4a = new List<Integer>();
+        List<Integer> testCase4b = new List<Integer>(9,new List<Integer>(8, new List<Integer>(7, new List<Integer>(5, new List<Integer>()))));
+
+        List<Integer> exceptedCase4 = new List<Integer>(9,new List<Integer>(8, new List<Integer>(7, new List<Integer>(5, new List<Integer>()))));
+
+        assertEquals(exceptedCase4, Worksheet1.merge(testCase4a,testCase4b));
+
+        // Test for the List b is empty
+        List<Integer> testCase5a = new List<Integer>(8,new List<Integer>(5,new List<Integer>(5, new List<Integer>(2, new List<Integer>()))));
+        List<Integer> testCase5b = new List<Integer>();
+
+        List<Integer> exceptedCase5 = new List<Integer>(8,new List<Integer>(5,new List<Integer>(5, new List<Integer>(2, new List<Integer>()))));
+
+        assertEquals(exceptedCase5, Worksheet1.merge(testCase5a,testCase5b));
+    }
+
+    @Test
+    @DisplayName("Tests for the Exercise 8")
+    public void test8() {
+        // Test for the List which all elements are same
+        List<Integer> testCase1 = new List<Integer>(9,new List<Integer>(9,new List<Integer>(9, new List<Integer>(9, new List<Integer>(9, new List<Integer>())))));
+
+        List<Integer> exceptedCase1 = new List<Integer>(9,new List<Integer>());
+
+        assertEquals(exceptedCase1, Worksheet1.removeDuplicates(testCase1));
+
+        // Test for the List in normal case
+        List<Integer> testCase2 = new List<Integer>(9,new List<Integer>(9,new List<Integer>(8, new List<Integer>(8, new List<Integer>(7, new List<Integer>())))));
+
+        List<Integer> exceptedCase2 = new List<Integer>(9,new List<Integer>(8, new List<>(7, new List<Integer>())));
+
+        assertEquals(exceptedCase2, Worksheet1.removeDuplicates(testCase2));
     }
 
     @AfterEach
@@ -41,5 +184,3 @@ public class Worksheet1Test {
 		System.out.println("Finished..." + testInfo.getDisplayName());
 	}
 }
-
-
